3347e2aa861aab95d6408f5c055181af292deb7c
Vitor Pantoja <vitortravassos3@gmail.com>
2022-03-11 18:33:55 -0300

    modal addition for pokemon detailed display

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,6 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active"><span  @click="highContrast()">teste</span></a>
-        </li>
       </ul>
       <form @submit.prevent="searchPokemons" class="d-flex">
         <input class="form-control me-2" type="search" aria-label="Search" v-model="search">
@@ -18,15 +15,14 @@
     </div>
   </div>
 </nav>
-
-  <div class="container">
+<br>
+<div class="container">
     <div class="row">
       <div class="col-12 col-md-3" v-for="(pokemon, index) in filteredPokemons" :key="pokemon.url" >
         <pokemon-card :index="index + 1" :name="pokemon.name" :url="pokemon.url" />
       </div>
+    </div>
   </div>
-  </div>
-  
 </template>
 
 <style scoped>
@@ -38,43 +34,6 @@
   background-size: contain;
   color: transparent;
 }
-.high-contrast {
-  background: black !important;
-  color: white !important;
-  border-color: white !important; }
-  .high-contrast nav {
-    background: black !important;
-    color: white !important;
-    border-color: white !important; }
-  .high-contrast div {
-    background: black !important;
-    color: white !important;
-    border-color: white !important;
-    border-bottom: 1px solid white; }
-  .high-contrast nav {
-    border-bottom: 1px solid white; }
-  .high-contrast body, .high-contrast h1, .high-contrast h2, .high-contrast h3, .high-contrast h4, .high-contrast h5, .high-contrast p, .high-contrast .contrast span {
-    color: white !important; }
-  .high-contrast option, .high-contrast body {
-    background: black !important;
-    color: yellow !important; }
-  .high-contrast td {
-    background: black !important;
-    color: white !important; }
-  .high-contrast img {
-    background: white !important;
-    color: yellow !important; }
-  .high-contrast ul, .high-contrast li {
-    background: black !important;
-    color: yellow !important; }
-  .high-contrast a, .high-contrast button {
-    background: black !important;
-    color: yellow !important;
-    border: 1px solid yellow !important; }
-  .high-contrast select, .high-contrast input {
-    background: black !important;
-    color: white !important;
-    border: 1px solid white !important; }
 </style>
 
 <script>
@@ -101,20 +60,12 @@ export default {
   methods: {
     searchPokemons: function() {
       this.filteredPokemons = this.pokemons;
-      if(this.search == '' || this.search == '  ') {
+      if(this.search == '' || this.search == ' ') {
         this.filteredPokemons = this.pokemons;
       } else {
         this.filteredPokemons = this.pokemons.filter((pokemon) => pokemon.name == this.search);
       }
     }
-  },
-  highContrast(){
-    if(!this.contrast){
-      document.body.classList.add('high-contrast');
-    }else{
-      document.body.classList.remove('high-contrast');
-    }
-    this.contrast = !this.contrast;
   }
 }
 </script>
--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,27 +1,44 @@
 <template>
-  <div class="card mb-3" style="max-width: 400px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img style="margin-top: 50px;" :src="currentImg" class="img-fluid rounded-start" alt="">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title"> {{ name}}</h5>
-          <p class="subtitle is-6">Tipo: {{ pokemon.type }}</p>
-          <p class="subtitle is-6">Altura: {{ pokemon.height / 10 }} M</p>
-          <p class="subtitle is-6">Peso: {{ pokemon.weight / 10 }} Kg</p>
+  <div class="card mb-3">
+    <div class="row g-0">
+      <div class="d-flex flex-wrap col-4">
+        <img :src="currentImg" class="img-fluid rounded-start" alt="">
       </div>
-    </div>
+      <div class="d-flex flex-wrap col-8">
+        <div class="card-body">
+          <h5 class="card-title"> <p class="capitalized">{{ name}}</p></h5>
+            <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+          <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+              <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalToggleLabel">Modal 1</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                  <p class="subtitle is-6">Tipo: {{ pokemon.type }}</p>
+                  <p class="subtitle is-6">Altura: {{ pokemon.height / 10 }} M</p>
+                  <p class="subtitle is-6">Peso: {{ pokemon.weight / 10 }} Kg</p>
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              </div>
+              </div>
+          </div>
+      </div>
+      <a class="btn btn-dark" data-bs-toggle="modal" href="#exampleModalToggle" role="button">detalhes</a>
+        </div>
+      </div> 
   </div>
+  
 </div>
-</template>
 
+</template>
 <script>
 import api from '../services/api';
+//import PokemonDetails from './PokemonDetails.vue';
 export default {
   name: 'PokemonCard',
   props: {
-    index: Number,
     name: String,
     url: String
   },
@@ -45,3 +62,8 @@ export default {
   },
 }
 </script>
+<style scoped>
+  .capitalized {
+	text-transform: capitalize;
+}
+</style>
